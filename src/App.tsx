c0beb3b32b98d7d607df671f20b97e1cf5dc07e7
MoskalenkoDev <kolya-4444@ukr.net>
2023-08-09 11:00:51 +0300

    idk

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 import UserList from "./components/UsersList";
+import img from './img.jpg';
 
 function App() {
+  
+  window.localStorage.setItem("image", img);
+
   return (
     <div className="App">
       <UserList />
